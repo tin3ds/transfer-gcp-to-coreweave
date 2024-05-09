94964813abf6e8c3a4faf8be64f47e0370da155d
tinnq <tinnq22@gmail.com>
2024-05-09 20:26:48 +0200

    chore: fix maxBuffer

--- script_file.js
+++ script_file.js
@@ -21,31 +21,23 @@ async function runScript() {
         }
 
         const cmdInfo = `gsutil du -s -ch gs://e3ds-master.appspot.com/${file}`;
-        const { stdout: infostdout, stderr: infostderr } = await exec(cmdInfo);
+        const { stdout: infostdout, stderr: infostderr } = await exec(cmdInfo, { maxBuffer: 1024 * 4000 });
         console.log("cmdDownload", infostdout, infostderr);
         const cmdDownload = `gsutil -m cp -r ${gcsBucket}/${file} ./${dirName}/`;
-        const { stdout, stderr } = await exec(cmdDownload);
+        const { stdout, stderr } = await exec(cmdDownload, { maxBuffer: 1024 * 4000 });
         console.log("cmdDownload", stdout, stderr);
         let cmdUpload = `aws s3 --endpoint=${s3Endpoint} cp ./${dirName}/${file} ${s3Bucket}/${file}`;
         if (file.includes("/")) {
           const fileNames = file.split("/");
           const fileName = fileNames[fileNames.length - 1];
           cmdUpload = `aws s3 --endpoint=${s3Endpoint} cp ./${dirName}/${fileName} ${s3Bucket}/${file}`;
-
-          const { stdout: uploadStout, stderr: uploadStderr } = await exec(
-            cmdUpload
-          );
-          console.log("cmdUpload", uploadStout, uploadStderr);
-          const cmdRemove = `rm -rf ${dirName}`;
-          await exec(cmdRemove);
-        } else {
-          const { stdout: uploadStout, stderr: uploadStderr } = await exec(
-            cmdUpload
-          );
-          console.log("cmdUpload", uploadStout, uploadStderr);
-          const cmdRemove = `rm -rf ${dirName}`;
-          await exec(cmdRemove);
         }
+        const { stdout: uploadStout, stderr: uploadStderr } = await exec(
+          cmdUpload, { maxBuffer: 1024 * 12000 }
+        );
+        console.log("cmdUpload", uploadStout, uploadStderr);
+        const cmdRemove = `rm -rf ${dirName}`;
+        await exec(cmdRemove, { maxBuffer: 1024 * 4000 });
 
         await fs.appendFile("./done-files.txt", `${file} \n`);
         console.log(`Done ${file}`);
